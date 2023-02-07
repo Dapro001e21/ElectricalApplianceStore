9bf5120b1f8ad7bdcd97029618c7b7e174d9955f
Dapro001e21 <Dapro004@gmail.com>
2023-02-07 16:40:49 +0300

    commit

--- ElectricalApplianceStore/Forms/AdminForm.Designer.cs
+++ ElectricalApplianceStore/Forms/AdminForm.Designer.cs
@@ -28,20 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.save_Button = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(439, 299);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // save_Button
+            // 
+            this.save_Button.Location = new System.Drawing.Point(12, 342);
+            this.save_Button.Name = "save_Button";
+            this.save_Button.Size = new System.Drawing.Size(175, 44);
+            this.save_Button.TabIndex = 1;
+            this.save_Button.Text = "Save";
+            this.save_Button.UseVisualStyleBackColor = true;
+            this.save_Button.Click += new System.EventHandler(this.save_Button_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Пользователи",
+            "Электроприборы"});
+            this.comboBox1.Location = new System.Drawing.Point(121, 10);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(150, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выберите таблицу:";
             // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.save_Button);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "AdminForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AdminForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button save_Button;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- ElectricalApplianceStore/Forms/AdminForm.cs
+++ ElectricalApplianceStore/Forms/AdminForm.cs
@@ -15,11 +15,157 @@ namespace ElectricalApplianceStore
     {
         SqlConnection connection;
         User user;
+        SqlDataAdapter userAdapter, electricalAppliancesAdapter;
+        DataTable userTable, electricalAppliancesTable;
+
         public AdminForm(SqlConnection connection, User user)
         {
             InitializeComponent();
             this.connection = connection;
             this.user = user;
+            comboBox1.SelectedIndex = 0;
+            userTable = new DataTable();
+            electricalAppliancesTable = new DataTable();
+            ShowTables();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    dataGridView1.DataSource = userTable;
+                    break;
+                case 1:
+                    dataGridView1.DataSource = electricalAppliancesTable;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ShowTables()
+        {
+            userTable.Rows.Clear();
+            electricalAppliancesTable.Rows.Clear();
+
+            userAdapter = new SqlDataAdapter("select * from Users", connection);
+            userAdapter.Fill(userTable);
+            electricalAppliancesAdapter = new SqlDataAdapter("select * from ElectricalAppliances", connection);
+            electricalAppliancesAdapter.Fill(electricalAppliancesTable);
+
+            comboBox1_SelectedIndexChanged(this, null);
+        }
+
+        private void save_Button_Click(object sender, EventArgs e)
+        {
+            InitCommands();
+            userAdapter.Update(userTable);
+            electricalAppliancesAdapter.Update(electricalAppliancesTable);
+            ShowTables();
+        }
+
+        public void InitCommands()
+        {
+            //Insert Users
+            SqlCommand insertCommandU = new SqlCommand("insert into Users values (@Name, @Email, @Password, @Type)", connection);
+            insertCommandU.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar, 50));
+            insertCommandU.Parameters.Add(new SqlParameter("@Email", SqlDbType.NVarChar, 50));
+            insertCommandU.Parameters.Add(new SqlParameter("@Password", SqlDbType.NVarChar, 50));
+            insertCommandU.Parameters.Add(new SqlParameter("@Type", SqlDbType.NVarChar, 30));
+
+            insertCommandU.Parameters["@Name"].SourceVersion = DataRowVersion.Current;
+            insertCommandU.Parameters["@Email"].SourceVersion = DataRowVersion.Current;
+            insertCommandU.Parameters["@Password"].SourceVersion = DataRowVersion.Current;
+            insertCommandU.Parameters["@Type"].SourceVersion = DataRowVersion.Current;
+
+            insertCommandU.Parameters["@Name"].SourceColumn = "Name";
+            insertCommandU.Parameters["@Email"].SourceColumn = "Email";
+            insertCommandU.Parameters["@Password"].SourceColumn = "Password";
+            insertCommandU.Parameters["@Type"].SourceColumn = "Type";
+
+            //Update Users
+            SqlCommand updateCommandU = new SqlCommand("update Users set Name = @Name, Email = @Email, Password = @Password, Type = @Type where Id = @Id", connection);
+            updateCommandU.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int));
+            updateCommandU.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar, 50));
+            updateCommandU.Parameters.Add(new SqlParameter("@Email", SqlDbType.NVarChar, 50));
+            updateCommandU.Parameters.Add(new SqlParameter("@Password", SqlDbType.NVarChar, 50));
+            updateCommandU.Parameters.Add(new SqlParameter("@Type", SqlDbType.NVarChar, 30));
+
+            updateCommandU.Parameters["@Id"].SourceVersion = DataRowVersion.Original;
+            updateCommandU.Parameters["@Name"].SourceVersion = DataRowVersion.Current;
+            updateCommandU.Parameters["@Email"].SourceVersion = DataRowVersion.Current;
+            updateCommandU.Parameters["@Password"].SourceVersion = DataRowVersion.Current;
+            updateCommandU.Parameters["@Type"].SourceVersion = DataRowVersion.Current;
+
+            updateCommandU.Parameters["@Id"].SourceColumn = "Id";
+            updateCommandU.Parameters["@Name"].SourceColumn = "Name";
+            updateCommandU.Parameters["@Email"].SourceColumn = "Email";
+            updateCommandU.Parameters["@Password"].SourceColumn = "Password";
+            updateCommandU.Parameters["@Type"].SourceColumn = "Type";
+
+            //Delete Users
+            SqlCommand deleteCommandU = new SqlCommand("delete from Users where Id = @Id", connection);
+            deleteCommandU.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int));
+            deleteCommandU.Parameters["@Id"].SourceVersion = DataRowVersion.Original;
+            deleteCommandU.Parameters["@Id"].SourceColumn = "Id";
+
+            userAdapter.InsertCommand = insertCommandU;
+            userAdapter.UpdateCommand = updateCommandU;
+            userAdapter.DeleteCommand = deleteCommandU;
+
+            //Insert ElectricalAppliances
+            SqlCommand insertCommandE = new SqlCommand("insert into ElectricalAppliances values (@Name, @DateOfRelease, @Supplier, @Price, @Weight)", connection);
+            insertCommandE.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar, 50));
+            insertCommandE.Parameters.Add(new SqlParameter("@DateOfRelease", SqlDbType.Date));
+            insertCommandE.Parameters.Add(new SqlParameter("@Supplier", SqlDbType.NVarChar, 50));
+            insertCommandE.Parameters.Add(new SqlParameter("@Price", SqlDbType.Money));
+            insertCommandE.Parameters.Add(new SqlParameter("@Weight", SqlDbType.Int));
+
+            insertCommandE.Parameters["@Name"].SourceVersion = DataRowVersion.Current;
+            insertCommandE.Parameters["@DateOfRelease"].SourceVersion = DataRowVersion.Current;
+            insertCommandE.Parameters["@Supplier"].SourceVersion = DataRowVersion.Current;
+            insertCommandE.Parameters["@Price"].SourceVersion = DataRowVersion.Current;
+            insertCommandE.Parameters["@Weight"].SourceVersion = DataRowVersion.Current;
+
+            insertCommandE.Parameters["@Name"].SourceColumn = "Name";
+            insertCommandE.Parameters["@DateOfRelease"].SourceColumn = "DateOfRelease";
+            insertCommandE.Parameters["@Supplier"].SourceColumn = "Supplier";
+            insertCommandE.Parameters["@Price"].SourceColumn = "Price";
+            insertCommandE.Parameters["@Weight"].SourceColumn = "Weight";
+
+            //Update ElectricalAppliances
+            SqlCommand updateCommandE = new SqlCommand("update Users set Name = @Name, Email = @Email, Password = @Password, Type = @Type where Id = @Id", connection);
+            updateCommandE.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int));
+            updateCommandE.Parameters.Add(new SqlParameter("@Name", SqlDbType.NVarChar, 50));
+            updateCommandE.Parameters.Add(new SqlParameter("@DateOfRelease", SqlDbType.Date));
+            updateCommandE.Parameters.Add(new SqlParameter("@Supplier", SqlDbType.NVarChar, 50));
+            updateCommandE.Parameters.Add(new SqlParameter("@Price", SqlDbType.Money));
+            updateCommandE.Parameters.Add(new SqlParameter("@Weight", SqlDbType.Int));
+
+            updateCommandE.Parameters["@Id"].SourceVersion = DataRowVersion.Original;
+            updateCommandE.Parameters["@Name"].SourceVersion = DataRowVersion.Current;
+            updateCommandE.Parameters["@DateOfRelease"].SourceVersion = DataRowVersion.Current;
+            updateCommandE.Parameters["@Supplier"].SourceVersion = DataRowVersion.Current;
+            updateCommandE.Parameters["@Price"].SourceVersion = DataRowVersion.Current;
+            updateCommandE.Parameters["@Weight"].SourceVersion = DataRowVersion.Current;
+
+            updateCommandE.Parameters["@Id"].SourceColumn = "Id";
+            updateCommandE.Parameters["@Name"].SourceColumn = "Name";
+            updateCommandE.Parameters["@DateOfRelease"].SourceColumn = "DateOfRelease";
+            updateCommandE.Parameters["@Supplier"].SourceColumn = "Supplier";
+            updateCommandE.Parameters["@Price"].SourceColumn = "Price";
+            updateCommandE.Parameters["@Weight"].SourceColumn = "Weight";
+
+            //Delete ElectricalAppliances
+            SqlCommand deleteCommandE = new SqlCommand("delete from ElectricalAppliances where Id = @Id", connection);
+            deleteCommandE.Parameters.Add(new SqlParameter("@Id", SqlDbType.Int));
+            deleteCommandE.Parameters["@Id"].SourceVersion = DataRowVersion.Original;
+            deleteCommandE.Parameters["@Id"].SourceColumn = "Id";
+
+            electricalAppliancesAdapter.InsertCommand = insertCommandE;
+            electricalAppliancesAdapter.UpdateCommand = updateCommandE;
+            electricalAppliancesAdapter.DeleteCommand = deleteCommandE;
         }
     }
 }
